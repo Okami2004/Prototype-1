99494bc29c5a1bad77d8421a2945e906bf71f08b
Logan-Okami <51331444+Okami2004@users.noreply.github.com>
2019-11-05 10:09:54 -0800

    finished camera

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5.0f;
     public float rotationSpeed;
     public float verticalInput;
+    public float horizontalinput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +16,15 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * speed * Time.deltaTime);
     }
 }
