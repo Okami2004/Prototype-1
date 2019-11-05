2d94e1e59fa8a011c58047b7cbc2fac6beb78ba2
Logan-Okami <51331444+Okami2004@users.noreply.github.com>
2019-11-05 09:09:00 -0800

    finished from the driving car and everything else to do with it

---
+++ Assets/scripts/FollowPlayerCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerCam : MonoBehaviour
+{
+    public GameObject player; 
+    private Vector3 offset = new Vector3(0, 11, -12);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //offset the camera by adding the players position
+      transform.position = player.transform.position + offset; 
+    }
+}
